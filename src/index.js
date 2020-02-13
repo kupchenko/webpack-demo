d1b9284400ad894f5afdfe8ceece65deff31c34e
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2020-02-13 22:35:38 +0200

    Webpack config: - Clean previous build files plugin - Copy html plugin

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import User from './user'
+
+const user = new User('Dmitrii', 23);
+
+console.log(user);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //Copies html files into output folder and inserts JS files
+const {CleanWebpackPlugin} = require('clean-webpack-plugin'); //Cleans files from previous build
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'out'),
         filename: '[name].[hash].bundle.js'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html' //If no template specified, empty index.html is created
+        }),
+        new CleanWebpackPlugin()
+    ]
 };
